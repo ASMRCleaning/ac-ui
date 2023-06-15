07923a55bc349b077ecb7dd2f8d6017c2d6c6be5
Mayara Rodrigues <mrodrigues21@myseneca.ca>
2023-06-14 21:33:24 -0400

    Adding jotai on ui project, and retrieve customer information

--- components/Header.js
+++ components/Header.js
@@ -2,24 +2,45 @@ import React from "react";
 import { Navbar, Nav, Form, Button, NavDropdown } from 'react-bootstrap';
 import { isAuthenticated, removeToken } from "../lib/authenticate";
 import { useRouter } from "next/router";
-
+import { useAtom } from "jotai";
+import { customerInfoAtom, userNameAtom } from "../store";
+import Link from 'next/link';
 
 const Header = (props) => {
+    //set userName global variable to show in header navbar
+    const [userName, setUserName] = useAtom(userNameAtom);
+    const [customerInfo, setCustomerInfo] = useAtom(customerInfoAtom);
     const router = useRouter();
-
-    // let token = readToken()
 
     function logout() {
         removeToken();
         router.push("/");
     }
 
+    //load customer info to display their name in header
+    async function customer() {
+        try {
+            const customer = await getCustomerInfo(userName);
+            setCustomerInfo({
+                'userId': customer.userId,
+                'firstName': customer.firstName,
+                'lastName': customer.lastName
+            });
+            router.push('/userHome');
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+    }
+
     return (
         <>
             <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <Nav className="container-fluid">
-                    <Navbar.Brand href="/">
-                        ASMR Cleaning Service
+                    <Navbar.Brand>
+                        <Link href="/" style={{ color: "#f0f5fa", textDecoration: "none" }}>
+                            ASMR Cleaning Service
+                        </Link>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarColor01"
                         aria-expanded="false"
@@ -27,45 +48,64 @@ const Header = (props) => {
                     <Navbar.Collapse id="navbarColor01">
                         <Nav className="navbar-nav me-auto">
                             <Nav.Item>
-                                <Nav.Link href="/">Home</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
                                 <NavDropdown title="About" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="/about"> About Us</NavDropdown.Item>
-                                    <NavDropdown.Item href="https://blog.nationwide.com/home/home-maintenance/how-to-clean-house-fast/">Blog</NavDropdown.Item>
+                                    <NavDropdown.Item>
+                                        <Link href="/about" style={{ textDecoration: "none" }}>About Us</Link></NavDropdown.Item>
+                                    <NavDropdown.Item href="https://blog.nationwide.com/home/home-maintenance/how-to-clean-house-fast/">
+                                        Blog
+                                    </NavDropdown.Item>
                                     <NavDropdown.Item href="https://www.goodhousekeeping.com/home/cleaning/g2550/best-cleaning-tips/">
-                                        Cleaning Tips</NavDropdown.Item>
+                                        Cleaning Tips
+                                    </NavDropdown.Item>
                                     <NavDropdown.Item href="https://www.bhg.com/homekeeping/house-cleaning/tips/eco-friendly-cleaning-ideas/">
-                                        Green Cleaning</NavDropdown.Item>
+                                        Green Cleaning
+                                    </NavDropdown.Item>
                                 </NavDropdown>
                             </Nav.Item>
                             <Nav.Item>
                                 <NavDropdown title="Services" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="/service"> All Service</NavDropdown.Item>
-                                    <NavDropdown.Item href="/service#house-cleaning">House Cleaning</NavDropdown.Item>
-                                    <NavDropdown.Item href="/service#movein-cleaning"> Move-in Cleaning</NavDropdown.Item>
-                                    <NavDropdown.Item href="/service#moveout-cleaning"> Move out Cleaning</NavDropdown.Item>
-                                    <NavDropdown.Item href="/service#green-cleaning"> Green Cleaning</NavDropdown.Item>
+
+                                    <NavDropdown.Item>
+                                        <Link style={{ textDecoration: "none" }} href="/service">All Service </Link>
+                                    </NavDropdown.Item>
+
+                                    <NavDropdown.Item>
+                                        <Link style={{ textDecoration: "none" }} href="/service#house-cleaning">House Cleaning
+                                        </Link>
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item>
+                                        <Link style={{ textDecoration: "none" }} href="/service#movein-cleaning"> Move-in Cleaning
+                                        </Link>
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item>
+                                        <Link style={{ textDecoration: "none" }} href="/service#moveout-cleaning"> Move out Cleaning
+                                        </Link></NavDropdown.Item>
+                                    <NavDropdown.Item>
+                                        <Link style={{ textDecoration: "none" }} href="/service#green-cleaning"> Green Cleaning
+                                        </Link></NavDropdown.Item>
                                 </NavDropdown>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link href="/contactUs">Contact us</Nav.Link>
+                                <Nav.Link>
+                                    <Link style={{ color: "#f0f5fa", textDecoration: "none" }} href="/contactUs"> Contact us </Link> </Nav.Link>
                             </Nav.Item>
                             <Nav.Item className="ml-auto">
                                 {!isAuthenticated() ? <Nav.Link href="/login">Login</Nav.Link> :
                                     <Nav.Item >
-                                    <NavDropdown title="Hi Customer" id="basic-nav-dropdown">
-
-
-                                        <NavDropdown.Item href="/userHome"> User Home Page</NavDropdown.Item>
-                                        <NavDropdown.Item href="/profile"> Profile</NavDropdown.Item>
-                                        <NavDropdown.Item href="/residence">Residence</NavDropdown.Item>
-                                        <NavDropdown.Item href="/residenceAddress">Address</NavDropdown.Item>
-
-                                        <NavDropdown.Item href="/booking"> Booking</NavDropdown.Item>
-                                        <NavDropdown.Item onClick={logout}> Logout</NavDropdown.Item>
-                                    </NavDropdown>
-                                </Nav.Item>}
+                                        <NavDropdown title={`Hi, ${customerInfo.firstName}`} id="basic-nav-dropdown">
+                                            <NavDropdown.Item onClick={customer}>  
+                                                <Link style={{ textDecoration: "none" }}> User Home Page </Link></NavDropdown.Item>
+                                            <NavDropdown.Item>
+                                                <Link style={{ textDecoration: "none" }} href="/profile"> Profile </Link></NavDropdown.Item>
+                                            <NavDropdown.Item>
+                                                <Link style={{ textDecoration: "none" }} href="/residence">Residence</Link></NavDropdown.Item>
+                                            <NavDropdown.Item>
+                                                <Link style={{ textDecoration: "none" }} href="/residenceAddress">Address</Link></NavDropdown.Item>
+                                            <NavDropdown.Item>
+                                                <Link style={{ textDecoration: "none" }} href="/booking"> Booking</Link></NavDropdown.Item>
+                                            <NavDropdown.Item onClick={logout}> Logout</NavDropdown.Item>
+                                        </NavDropdown>
+                                    </Nav.Item>}
                             </Nav.Item>
                             <Nav.Item>
                             </Nav.Item>
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/bootstrap.min.css";
 import Layout from "../components/Layout";
 import RouteGuard from "../components/RouteGuard";
-import { useState } from "react";
+import { Provider } from "jotai";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <Provider>
       <RouteGuard>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </RouteGuard>
+      </Provider>
     </>
   );
 }
